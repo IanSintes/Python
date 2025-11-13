2873e509d85d2e5b113b85ec56d27886a5816e83
IanSintes <isintes21197@iesjoanramis.org>
2025-11-13 12:10:26 +0100

    Ex 13

--- Exercici 13.py
+++ Exercici 13.py
@@ -1,16 +1,27 @@
+opcio = float(input("""Escriu una opció:
+                    1. Suma
+                    2. Resta
+                    3. Multiplicació
+                    4. Divisió
+                    5. Resto
+                    6. Quocient
+                    7.Potencia
+                    0. Sortir
+                    """))
 A = float(input("Insereix el primer nombre: "))
 B = float(input("Insereix el segon nombre: "))
-C = A + B
-D = A - B
-E = A * B
-F = A / B
-G = A % B
-H = A // B
-I = A ** B
-print("La suma de {} + {} és {}".format(A, B, C))
-print("La resta de {} - {} és {}".format(A, B, D))
-print("La multiplicació de {} * {} és {}".format(A, B, E))
-print("La divisió de {} / {} és {}".format(A, B, F))
-print("El resto de {} % {} és {}".format(A, B, G))
-print("La quocient de {} // {} és {}".format(A, B, H))
-print("La potencia de {} ** {} és {}".format(A, B, I))
+
+if opcio==1:
+    print("La suma de {} + {} és {}".format(A, B, A+B))
+elif opcio==2:
+    print("La resta de {} - {} és {}".format(A, B, A-B))
+elif opcio==3:
+    print("La multiplicació de {} * {} és {}".format(A, B, A*B))
+elif opcio==4:
+    print("La divisió de {} / {} és {}".format(A, B, A/B))
+elif opcio==5:
+    print("El resto de {} % {} és {}".format(A, B, A%B))
+elif opcio==6:
+    print("La quocient de {} // {} és {}".format(A, B, A//B))
+elif opcio==7:
+    print("La potencia de {} ** {} és {}".format(A, B, A**B))
