d3020aaf162d8ccb1734af615e1477982d52c085
IanSintes <isintes21197@iesjoanramis.org>
2025-11-13 11:51:57 +0100

    Ex 12

--- Exercici 12.py
+++ Exercici 12.py
@@ -1,7 +1,12 @@
-edat = float(input("Insereix la seva edat: "))
-if edat >18:
-    print("Ets major d'edat, ves a treballar")
-elif edat <18:
-    print("Ets menor d'edat, ves a estudiar")
-else:
-    print("Tens 18 anys, disfruta!")
+def majoredat(edat):
+    if edat >18:
+        print("Ets major d'edat, ves a treballar")
+    elif edat <18:
+        print("Ets menor d'edat, ves a estudiar")
+    else:
+        print("Tens 18 anys, disfruta!")
+
+edat = int(input("Insereix la seva edat: "))
+majoredat(edat)
+edat = int(input("Insereix la seva edat: "))
+majoredat(edat)
