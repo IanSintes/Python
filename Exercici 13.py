cb42450ad84a0a5c43b7d22a57106e05b1a356b8
IanSintes <isintes21197@iesjoanramis.org>
2025-11-13 11:43:58 +0100

    Ex 13

---
+++ Exercici 13.py
@@ -0,0 +1,16 @@
+A = float(input("Insereix el primer nombre: "))
+B = float(input("Insereix el segon nombre: "))
+C = A + B
+D = A - B
+E = A * B
+F = A / B
+G = A % B
+H = A // B
+I = A ** B
+print("La suma de {} + {} és {}".format(A, B, C))
+print("La resta de {} - {} és {}".format(A, B, D))
+print("La multiplicació de {} * {} és {}".format(A, B, E))
+print("La divisió de {} / {} és {}".format(A, B, F))
+print("El resto de {} % {} és {}".format(A, B, G))
+print("La quocient de {} // {} és {}".format(A, B, H))
+print("La potencia de {} ** {} és {}".format(A, B, I))
