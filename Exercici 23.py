bdbee9e2fbaf9d15787f9207f2534c0fdad8f708
IanSintes <isintes21197@iesjoanramis.org>
2025-11-19 13:45:50 +0100

    Ex 23

---
+++ Exercici 23.py
@@ -0,0 +1,8 @@
+def crear_repetits(n, car):
+    return car * n
+
+# Exemples de prova
+print(crear_repetits(5, "a"))
+print(crear_repetits(3, "x"))
+print(crear_repetits(0, "b"))
+print(crear_repetits(7, "*"))
