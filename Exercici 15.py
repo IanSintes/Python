3d3c269632ad29ef5e975e61d00b8398e7fa6d10
IanSintes <isintes21197@iesjoanramis.org>
2025-11-19 12:29:59 +0100

    Ex 15

---
+++ Exercici 15.py
@@ -0,0 +1,11 @@
+def gran(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print("Exemples de prova de la funció gran():")
+print("gran(5, 3) =", gran(5, 3))
+print("gran(10, 20) =", gran(10, 20))
+print("gran(-4, -9) =", gran(-4, -9))
+print("gran(7.5, 7.5) =", gran(7.5, 7.5))
