0e861487598ab93137c2036fde9c4050fbad787e
IanSintes <isintes21197@iesjoanramis.org>
2025-11-19 12:51:52 +0100

    Ex 18

---
+++ Exercici 18.py
@@ -0,0 +1,9 @@
+def es_vocal(car):
+    return car.lower() in 'aeiou'
+
+# Exemples de prova
+print("es_vocal('a') =", es_vocal('a'))
+print("es_vocal('E') =", es_vocal('E'))
+print("es_vocal('b') =", es_vocal('b'))
+print("es_vocal('O') =", es_vocal('O'))
+print("es_vocal('z') =", es_vocal('z'))
