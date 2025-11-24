002f8f7e6408eae4ea17a586a00218f8c68abc68
IanSintes <isintes21197@iesjoanramis.org>
2025-11-24 09:45:07 +0100

    Ex 17

--- Exercici 17.py
+++ Exercici 17.py
@@ -1,8 +1,5 @@
-def longitud(obj):
-    comptador = 0
-    for _ in obj:
-        comptador += 1
-    return comptador
+def longitud(a):
+    return len(a)
 
 # Exemples de prova
 print("longitud([1, 2, 3, 4]) =", longitud([1, 2, 3, 4]))
@@ -10,3 +7,11 @@ print("longitud('Hola món') =", longitud('Hola món'))
 print("longitud([]) =", longitud([]))
 print("longitud('') =", longitud(''))
 print("longitud([10, 20, 30]) =", longitud([10, 20, 30]))
+
+#Codig fet amb en Joan
+a=[1, 3, 4, "Pere", [3,4], "a", [1,[3,4], 5]]
+print(longitud(a))
+b=("Soc Ian Sintes. Que tal estas?")
+print(longitud(b))
+c=[]
+print(longitud(c))
