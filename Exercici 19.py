86909e46230a2b5355b66415d5f722306411b432
IanSintes <isintes21197@iesjoanramis.org>
2025-11-25 10:07:25 +0100

    Ex 19

--- Exercici 19.py
+++ Exercici 19.py
@@ -6,7 +6,7 @@ def sumar_llista(llista):
 
 def multiplicar_llista(llista):
     if not llista:
-        return 0  # Retorna 0 si la llista està buida
+        return 0  
     total = 1
     for num in llista:
         total *= num
@@ -18,3 +18,8 @@ print("sumar_llista([5, 10, 2]) =", sumar_llista([5, 10, 2]))
 print("multiplicar_llista([1, 2, 3, 4]) =", multiplicar_llista([1, 2, 3, 4]))
 print("multiplicar_llista([5, 10, 2]) =", multiplicar_llista([5, 10, 2]))
 print("multiplicar_llista([]) =", multiplicar_llista([]))
+
+#Programa Principal (Programa fet amb en Joan)
+a = [1,3,5,7,10]
+print("La suma dels elements de la llista {} val {}".format(a,sumar_llista(a)))
+print("La multiplicacó dels elements de la llista {} val {}".format(a,multiplicar_llista(a)))
