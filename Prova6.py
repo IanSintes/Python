d57456d994a5895592a66b88cc0af017f80c8f4f
IanSintes <isintes21197@iesjoanramis.org>
2025-11-26 12:47:48 +0100

    prova6

---
+++ Prova6.py
@@ -0,0 +1,17 @@
+a = {"1":"H", "2":"o", "3":"l", "4":"a", "5":",", "6":[1,2,3,4]}
+#b = a.pop("6")
+#b = a.popitem()
+b = {"7":"pepe","8":"Miquel"}
+a.update(b)
+print(b)
+print(a)
+
+
+""""
+#b = a.keys()
+for e in a:
+    print(a[e])
+
+for c,v in a.items():
+    print("{}:{}, ".format(c,v))
+"""
