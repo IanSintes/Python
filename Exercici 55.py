d8a4cbca5ac621d2b2a6c2bff1ffa14a8cc700a5
IanSintes <isintes21197@iesjoanramis.org>
2025-12-04 17:24:54 +0100

    Ex 55

---
+++ Exercici 55.py
@@ -0,0 +1,11 @@
+# Nombres parells
+print("Nombres parells fins a 100:")
+for i in range(2, 101, 2):
+    print(i, end=" ")
+print("\n")
+
+# Nombres senars
+print("Nombres senars fins a 100:")
+for i in range(1, 101, 2):
+    print(i, end=" ")
+print()
