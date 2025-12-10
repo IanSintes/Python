a2a6cfd75339e4642035ad5e77fe9746a1b00240
IanSintes <isintes21197@iesjoanramis.org>
2025-12-10 13:59:55 +0100

    Prova 13

--- Prova 13.py
+++ Prova 13.py
@@ -1,11 +1,20 @@
 # Fitxers
+l = [50,100,200]
+with open("Prova.txt","w") as f:
+    f.write(str(l))
+with open("Prova.txt","r") as f:
+    linias = f.readlines()
+    linias = [n[:-1] for n in linias]
+    print(linias)
+
 """
 f = open("Prova.txt","r")
 print(f.read())
 f.close()
-"""
+
 
 with open("Prova.txt","r") as f:
     linias = f.readlines()
     linias = [n[:-1] for n in linias]
     print(linias)
+    """
